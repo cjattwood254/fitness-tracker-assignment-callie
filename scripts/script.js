fff097542e86e125fb4af028a3e3b43f8c7de42f
Callie <cjattwood254@gmail.com>
2024-03-19 16:00:23 -0500

    changed input type and adjusted some names

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
+// array to store types of exercise
 const logs = [
-    {name: "30 Second Hamstring Stretch", type: "Stretch", reps: 2},
-    {name: "Pushups", type: "Strength", reps: 10},
-    {name: "Jumping Jacks", type: "Stretching", reps: 30},
-    {name: "Hip Flexor Stretch", type: "Stretch", reps: 4}
+    {name: "30 Second Hamstring Stretch", type: "Stretch", duration: "Two 30-second reps"},
+    {name: "Pushups", type: "Strength", duration: "10 reps"},
+    {name: "Jumping Jacks", type: "Stretching", duration: "30 reps"},
+    {name: "Hip Flexor Stretch", type: "Stretch", duration: "Two 30-second reps each side"}
 ];
+
+
+// workout summary calculation (come back to later?)
+function calculateTotalDuration(durationArray){
+    
+
+// create a Workout Class
