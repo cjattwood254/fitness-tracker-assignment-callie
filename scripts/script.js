84dec22e686c7c149f2d30f34f00e599185ee5fd
Callie <cjattwood254@gmail.com>
2024-03-19 15:47:15 -0500

    created an array to store different exercises

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const logs = [
+    {name: "30 Second Hamstring Stretch", type: "Stretch", reps: 2},
+    {name: "Pushups", type: "Strength", reps: 10},
+    {name: "Jumping Jacks", type: "Stretching", reps: 30},
+    {name: "Hip Flexor Stretch", type: "Stretch", reps: 4}
+];
